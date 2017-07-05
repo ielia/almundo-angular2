c8697bbba834140d39b8aec4eac2a74b552779a7
Ignacio Elia <ignacio.elia@hyatt.com>
2017-07-05 03:12:43 -0300

    Added support for 'advertised' flag. Also improved styles greatly.

--- src/app/models/hotel.ts
+++ src/app/models/hotel.ts
@@ -1,14 +1,32 @@
+class Location {
+  constructor(
+    public address: String,
+    public city: String,
+    public country: String,
+    public lat: Number,
+    public lng: Number
+  ) {}
+}
+
+class Price {
+  constructor(
+    public currency: String,
+    public amount: Number
+  ) {}
+}
+
 export class Hotel {
   constructor(
     public created: Date,
     public updated: Date,
     public name: String,
-    public location: object,
+    public location: Location,
     public stars: Number,
     public image: String,
     public amenities: String[],
-    public price: object,
+    public price: Price,
     public discount: Number,
-    public paymentTypes: String[]
+    public paymentTypes: String[],
+    public advertised: Boolean
   ) {}
 }
